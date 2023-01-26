21a5275068eaad1893e2446798dac8568b43e63e
matthewkolden <matthewkolden2k@gmail.com>
2023-01-26 03:52:47 -0500

    Test socket.io implementation

--- views/Index.jsx
+++ views/Index.jsx
@@ -9,13 +9,40 @@ class Index extends React.Component {
         <head>
           <title>{title}</title>
           <link rel="stylesheet" href="/stylesheets/style.css" />
+          <script defer src="http://localhost:3000/socket.io/socket.io.js" />
+          <script defer src="javascripts/script.js" />
         </head>
         <body>
-          <h1>{title}</h1>
-          <p>Welcome to {title}</p>
+          <div>
+            <header>
+              <h1>Chat</h1>
+            </header>
+            <main>
+              <div>
+                <h3>Room Name:</h3>
+                <h2 id="room-name"></h2>
+                <h3>Users</h3>
+                <ul id="users"></ul>
+              </div>
+              <div></div>
+            </main>
+            <div>
+              <form id="chat-form">
+                <input
+                  id="msg"
+                  type="text"
+                  placeholder="Enter Message"
+                  required
+                  autoComplete="off"
+                />
+                <button>Send</button>
+              </form>
+            </div>
+          </div>
         </body>
       </html>
     )
   }
 }
+
 module.exports = Index
